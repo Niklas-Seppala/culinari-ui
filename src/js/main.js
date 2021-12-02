9b7c6ab7de1f6446972ebd700c5d023a037162e6
Niklas-Seppala <niklas.t.seppala@gmail.com>
2021-12-02 20:27:58 +0200

    test/demo

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,18 @@ import { TopMenuView } from "./views/TopMenu/TopMenuView";
 import { UserMenuView } from "./views/UserMenu/UserMenuView";
 
 const main = () => {
-  const main = document.getElementsByTagName('main').item(0)
 
   const topMenu = new TopMenuView(document.getElementById('top-panel-view'));
+  // Content navigation clicks.
   topMenu.contentNav.on.talkedClicked(e => console.log('talked') )
   topMenu.contentNav.on.likedClicked(e => console.log('liked'))
   topMenu.contentNav.on.latestClicked(e => console.log('latest'))
-
+  // Search and menu clicks.
   topMenu.on.searchClicked(e => console.log('search'))
   topMenu.on.userMenuClicked(e => console.log('menu'))
 
-  const userMenu = new UserMenuView(main);
+  const userMenu = new UserMenuView(document.getElementsByTagName('main').item(0));
+  // Set profile state.
   userMenu.profile.render({
     username: 'Markoboy',
     avatar: 'https://yt3.ggpht.com/ytc/AKedOLQe8H7ugXR3Y054TX2CXg5iY1ojDFB6Qoes2b1n=s900-c-k-c0x00ffffff-no-rj',
@@ -21,6 +22,7 @@ const main = () => {
     forks: 666
   })
 
+  // Menu click events.
   userMenu.anonymous.on.aboutClicked(e => console.log('about'))
   userMenu.anonymous.on.loginClicked(e => console.log('login'))
   userMenu.anonymous.on.registerClicked(e => console.log('register'))
@@ -29,6 +31,7 @@ const main = () => {
   userMenu.logged.on.myRecipesClicked(e => console.log('my recipes'))
   userMenu.logged.on.newRecipeClicked(e => console.log('new recipe'))
 
+  // Detach anonymous part from user menu.
   userMenu.anonymous.detach();
 };
 

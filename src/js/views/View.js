85725116d8b69461ba5ffc11ce34bd51d12caa69
Niklas-Seppala <niklas.t.seppala@gmail.com>
2021-12-03 16:01:43 +0200

    implement closed food post

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -20,6 +20,8 @@ export class View {
   }
 
   attach(parent) {
+    this.root.classList.remove('display-none')
+
     if (!parent) {
       // No new parent specified, try to attach to current parent.
       this.parent.appendChild(this.root);
@@ -68,3 +70,62 @@ export class View {
     return result;
   }
 }
+
+const icon = {
+  type: {
+    LIKE: 0,
+    COMMENT: 1,
+    FORK: 2,
+    TIME: 3,
+    EXPAND: 4,
+  },
+  src: [
+    ['../icons/heart.png', 'like'],
+    ['../icons/comment.png', 'comment'],
+    ['../icons/fork.png', 'fork'],
+    ['../icons/time.png', 'duration'],
+    ['../icons/more-dots.png', 'expand'],
+  ],
+  size: {
+    SMALL: 'icon-small',
+    TINY: 'icon-tiny',
+    MEDIUM: 'icon-medium',
+    LARGE: 'icon-large',
+    HUGE: 'icon-huge'
+  },
+
+  labeled: (type, size, css) => {
+    const root = document.createElement('div');
+    root.classList.add('icon-label', 'column');
+
+    const iconElement = document.createElement('img');
+    
+    iconElement.classList.add('icon')
+    iconElement.classList.add(size)
+    css?.forEach(c => iconElement.classList.add(c))
+    iconElement.src = icon.src[type][0];
+    iconElement.alt = icon.src[type][1];
+
+    const label = document.createElement('span');
+    label.classList.add(size)
+
+    root.appendChild(iconElement);
+    root.appendChild(label);
+    return { root, label };
+  },
+  plain: (type, size, css) => {
+    const root = document.createElement('div');
+    const iconElem = document.createElement('img');
+
+    iconElem.classList.add('icon')
+    iconElem.classList.add(size)
+
+    css?.forEach(c => iconElem.classList.add(c))
+    iconElem.src = icon.src[type][0];
+    iconElem.alt = icon.src[type][1];
+    root.appendChild(iconElem);
+    return [root];
+  }
+};
+
+export {icon};
